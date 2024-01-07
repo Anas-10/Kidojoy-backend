bc0517f69ddd1126ca36ebc27c656bcc20fd32e4
Anas-10 <amira.arafa@dis4u-consulting.com>
2024-01-07 19:42:51 +0100

    config

--- src/config.js
+++ src/config.js
@@ -12,18 +12,7 @@ config.database = {
    
 }
 
-config.auth = [
-    {
-        TOKEN_KEY: 'manager.kido.joy',
-        role: 'MANAGER',
-        username: 'manager.kido',
-        //RPUWDX6B
-        password: '$2a$10$tZ2F76dXZAKvnu0H3SKuNek1fWxDIPYF35ym75GUQqnC04BpwCVmm'
-    },
-    
-]
 
-config.PORT = '3002'
 
 
 module.exports = config
